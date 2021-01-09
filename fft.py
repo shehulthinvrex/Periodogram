4b782a4c57c4e848498da35c634246055b44c913
shehulthinvrex <42564638+shehulthinvrex@users.noreply.github.com>
2021-01-09 12:57:22 +0530

    Update fft.py
    I* think this much is enough

--- fft.py
+++ fft.py
@@ -1,32 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 20 08:49:52 2020
-
-@author: subeesh
-"""
-
-import numpy as np
-
-#Main code for the periodogram
-import matplotlib.pyplot as plt
-
-
-"""
-https://www.ritchievink.com/blog/2017/04/23/understanding-the-fourier-transform-by-example/
-"""
-
-def DFT(x):
-    """
-    Compute the discrete Fourier Transform of the 1D array x
-    :param x: (array)
-    """
-
-    N = x.size
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    e = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(e, x),N
 
 import numpy as np
 def npfft(x,t):
